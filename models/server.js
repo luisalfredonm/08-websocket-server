69102f9da3f0ae6753dc1feea2eee856517d41e0
Luis <lualfredo@gmail.com>
2023-09-12 20:18:39 -0600

    conectar y desconectar usuario

--- models/server.js
+++ models/server.js
@@ -5,6 +5,9 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.server = require('http').createServer(this.app);
+    this.io = require('socket.io')(this.server);
+
 
     this.paths = {};
 
@@ -13,6 +16,8 @@ class Server {
 
     //rutas de mi aplicacion
     this.routes();
+
+    this.sockets();
   }
 
   //Middlewares
@@ -29,8 +34,20 @@ class Server {
     // this.app.use(this.paths.auth, require("../routes/auth"));
   }
 
+  //Sockets implemtacion de soc
+
+  sockets() {
+    this.io.on('connection', socket => {
+      console.log('Cliente conectado', socket.id);
+
+      socket.on('disconnect',() => {
+        console.log('Cliente desconectado', socket.id)
+      })
+    })
+  }
+
   listen() {
-    this.app.listen(this.port, () => {
+    this.server.listen(this.port, () => {
       console.log("Servidor corriendo en puerto", this.port);
     });
   }
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+
+
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
+
+
+const socket = io();
+
+
+socket.on('connect', () => {
+    console.log('conectado');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    console.log('desconectado');
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+});
